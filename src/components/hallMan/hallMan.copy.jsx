019b3032963cbd993df6de10e80a8babd70baeb3
llYuliall <55938576+llYuliall@users.noreply.github.com>
2019-10-13 18:08:17 +0800

    Update hallMan.copy.jsx

--- src/components/hallMan/hallMan.copy.jsx
+++ src/components/hallMan/hallMan.copy.jsx
@@ -31,11 +31,11 @@ class HallMan extends React.Component
 										</section>
 									</div>
 									<div class="p-0 pb-3 p-sm-3 col-sm-6 col-12 flexDown">
-										<a href="#" class="image featuring"><img src="./web/homepage/tutor_photo/Lucas.JPG" alt="" /></a>
+										<a href="#" class="image featuring"><img src="./web/homepage/tutor_photo/Eugene.JPG" alt="" /></a>
 										<section class="box no-box">
 											<header>
-												<h3>Lucus QI</h3>
-												<p>1 / F</p>
+												<h3>Eugene LOW</h3>
+												<p>1 - 2 / F</p>
 											</header>
 
 											{/* <footer>
@@ -57,11 +57,11 @@ class HallMan extends React.Component
 							<section class="container">
 								<div class="row"> */}
 									<div class="p-0 pb-3 p-sm-3 col-sm-6 col-12 flexDown">
-										<a href="#" class="image featuring"><img src="./web/homepage/tutor_photo/Naeem.JPEG" alt="" /></a>
+										<a href="#" class="image featuring"><img src="./web/homepage/tutor_photo/Alex.JPG" alt="" /></a>
 										<section class="box no-box">
 											<header>
-												<h3>NAEEM Muhammad</h3>
-												<p>2 / F</p>
+												<h3>Alex XIAO</h3>
+												<p>3 / F</p>
 											</header>
 
 											{/* <footer>
@@ -72,22 +72,7 @@ class HallMan extends React.Component
 											</footer> */}
 										</section>
 									</div>
-									{/* <div class="p-0 pb-3 p-sm-3 col-sm-6 col-12 flexDown">
-											<a href="#" class="image featuring"><img src="./web/homepage/tutor_photo/Julie.JPEG" alt="" /></a>
-										<section class="box no-box">
-											<header>
-												<h3>Julie YOU</h3>
-												<p>3 - 4 / F </p>
-											</header> */}
-
-									{/* <footer>
-												<ul class="actions">
-													<li><a target="_blank" href="https://mp.weixin.qq.com/s/ZJDl2b36pg_GmotgTJggLQ" class="button icon fa-file-text">Continue Reading</a></li>
-													<li><a href="#" class="button alt icon fa-comment">33 comments</a></li>
-												</ul>
-											</footer> */}
-									{/* </section>
-									</div> */}
+								     
 									{/* </div>
 							</section>
 
@@ -99,11 +84,11 @@ class HallMan extends React.Component
 							<section>
 								<div class="row"> */}
 									<div class="p-0 pb-3 p-sm-3 col-sm-6 col-12 flexDown">
-										<a href="#" class="image featuring"><img src="./web/homepage/tutor_photo/jiwon.JPG" alt="" /></a>
+										<a href="#" class="image featuring"><img src="./web/homepage/tutor_photo/Naeem.JPG" alt="" /></a>
 										<section className="box no-box">
 											<header>
-												<h3>JEONG Jiwon</h3>
-												<p>5 / F</p>
+												<h3>NAEEM Muhammad</h3>
+												<p>4 / F</p>
 											</header>
 											{/* <footer>
 												<ul class="actions">
@@ -114,38 +99,38 @@ class HallMan extends React.Component
 										</section>
 									</div>
 									<div class="p-0 pb-3 p-sm-3 col-sm-6 col-12 flexDown">
-										<a href="#" class="image featuring "><img src="./web/homepage/tutor_photo/victor.JPG" alt="" /></a>
+										<a href="#" class="image featuring "><img src="./web/homepage/tutor_photo/Andrew.JPG" alt="" /></a>
 										<section class="box no-box">
+											<header>
+												<h3>Andrew LI</h3>
+												<p>5 / F</p>
+											</header>{/* <footer>
+												<ul class="actions">
+													<li><a target="_blank" href="https://mp.weixin.qq.com/s/ZdTJF2iJLlwYksYC-G8bww" class="button icon fa-file-text">Continue Reading</a></li>
+													<li><a href="#" class="button alt icon fa-comment">33 comments</a></li>
+												</ul>
+											</footer> */}
+										</section>
+									</div>
+									{/* </div>
+							</section>
+
+						</div>
+					</div>
+				
+				
+
+					<div className="row margin-top">
+						<div class="col-12">
+
+							<section>
+								<div class="row"> */}
+									<div class="p-0 pb-3 p-sm-3 col-sm-6 col-12 flexDown">
+										<a href="#" class="image featuring"><img src="./web/homepage/tutor_photo/victor.JPG" alt="" /></a>
+										<section className="box no-box">
 											<header>
 												<h3>Victor LUK</h3>
 												<p>6 - 7 / F</p>
-											</header>{/* <footer>
-												<ul class="actions">
-													<li><a target="_blank" href="https://mp.weixin.qq.com/s/ZdTJF2iJLlwYksYC-G8bww" class="button icon fa-file-text">Continue Reading</a></li>
-													<li><a href="#" class="button alt icon fa-comment">33 comments</a></li>
-												</ul>
-											</footer> */}
-										</section>
-									</div>
-									{/* </div>
-							</section>
-
-						</div>
-					</div>
-				
-				
-
-					<div className="row margin-top">
-						<div class="col-12">
-
-							<section>
-								<div class="row"> */}
-									<div class="p-0 pb-3 p-sm-3 col-sm-6 col-12 flexDown">
-										<a href="#" class="image featuring"><img src="./web/homepage/tutor_photo/Chriss.JPEG" alt="" /></a>
-										<section className="box no-box">
-											<header>
-												<h3>Chriss CHIN</h3>
-												<p>8 - 9 / F</p>
 											</header>
 											{/* <footer>
 												<ul class="actions">
@@ -156,11 +141,11 @@ class HallMan extends React.Component
 										</section>
 									</div>
 									<div class="p-0 pb-3 p-sm-3 col-sm-6 col-12 flexDown">
-										<a href="#" class="image featuring "><img src="./web/homepage/tutor_photo/CK.JPG" alt="" /></a>
+										<a href="#" class="image featuring "><img src="./web/homepage/tutor_photo/Chriss.JPG" alt="" /></a>
 										<section class="box no-box">
 											<header>
-												<h3>CK KHOO</h3>
-												<p>10 / F</p>
+												<h3>Chriss CHIN</h3>
+												<p>8 - 9 / F</p>
 											</header>{/* <footer>
 												<ul class="actions">
 													<li><a target="_blank" href="https://mp.weixin.qq.com/s/ZdTJF2iJLlwYksYC-G8bww" class="button icon fa-file-text">Continue Reading</a></li>
@@ -182,11 +167,11 @@ class HallMan extends React.Component
 							<section>
 								<div class="row"> */}
 									<div class="p-0 pb-3 p-sm-3 col-sm-6 col-12 flexDown">
-										<a href="#" class="image featuring"><img src="./web/homepage/tutor_photo/Suzy.JPG" alt="" /></a>
+										<a href="#" class="image featuring"><img src="./web/homepage/tutor_photo/CK.JPG" alt="" /></a>
 										<section className="box no-box">
 											<header>
-												<h3>Suzy ZHENG</h3>
-												<p>11 / F</p>
+												<h3>CK KHOO</h3>
+												<p>10 / F</p>
 											</header>
 											{/* <footer>
 												<ul class="actions">
@@ -199,7 +184,22 @@ class HallMan extends React.Component
 
 								</div>
 							</section>
+                            <div class="p-0 pb-3 p-sm-3 col-sm-6 col-12 flexDown">
+                                <a href="#" class="image featuring"><img src="./web/homepage/tutor_photo/SzeSze.JPEG" alt="" /></a>
+                                <section class="box no-box">
+                                    <header>
+                                        <h3>SzeSze LAM</h3>
+                                        <p>11 / F </p>
+                                    </header>
 
+                                    {/* <footer>
+												<ul class="actions">
+													<li><a target="_blank" href="https://mp.weixin.qq.com/s/ZJDl2b36pg_GmotgTJggLQ" class="button icon fa-file-text">Continue Reading</a></li>
+													<li><a href="#" class="button alt icon fa-comment">33 comments</a></li>
+												</ul>
+											</footer> */}
+                                </section>
+                            </div>
 						</div>
 					</div>
 
@@ -223,84 +223,84 @@ class HallMan extends React.Component
 							<h2 className="col-12">RESIDENCE TUTOR</h2>
 							<div className="col-12 container">
 								<div className="row" id="RTimg">
-									<div className="col-md-6 col-12 p-0 m-0 my-auto d-flex justify-content-md-start justify-content-center flex-row-reverse pr-1 ">
-									
-										<img className="RTimg" src="./web/homepage/tutor_photo/Lucas.JPG" alt="" />
-										<div className="RTinfo">
-											<div className="w-100"><div className="triangle"></div><div className="title"><span>101B</span> / Lucas</div></div>
-											<div className="info">PEK Soccer Competition, Returning Scheme</div>
-										</div>
-									</div>
-									<div className="col-md-6 col-12 p-0 m-0 my-auto d-flex justify-content-md-start justify-content-center pr-1 goDown">
-									
-										<img className="RTimg" src="./web/homepage/tutor_photo/Naeem.JPEG" alt="" />
-										<div className="RTinfo RTinfoR">
-											<div className="w-100 ">
-											
-											<div className="title titleR">Naeem \ <span>201A</span></div><div className="triangle triangleR"></div></div>
-											<div className="info infoR">PEK Photography Competition, Discipline</div>
-										</div>
-									</div>
-									</div>
-									<div className="row" id="RTimg">
-									<div className="col-md-6 col-12 p-0 m-0 my-auto d-flex justify-content-md-start justify-content-center flex-row-reverse pr-1 ">
-									
-										<img className="RTimg" src="./web/homepage/tutor_photo/Sherry.JPG" alt="" />
-										<div className="RTinfo">
-											<div className="w-100"><div className="triangle"></div><div className="title"><span>301A</span> / Sherry</div></div>
-											<div className="info">PEK Athletic Meet, Activity Coordinator</div>
-										</div>
-									</div>
-									<div className="col-md-6 col-12 p-0 m-0 my-auto d-flex justify-content-md-start justify-content-center pr-1 goDown">
-									
-										<img className="RTimg" src="./web/homepage/tutor_photo/jiwon.JPG" alt="" />
-										<div className="RTinfo RTinfoR">
-											<div className="w-100 ">
-											
-											<div className="title titleR">Jiwon \ <span>501A</span></div><div className="triangle triangleR"></div></div>
-											<div className="info infoR">PEK Singing Competition, Hall  Book Coordinator, RA/SRO Liaison</div>
-										</div>
-									</div>
-									</div>
-									<div className="row" id="RTimg">
-									<div className="col-md-6 col-12 p-0 m-0 my-auto d-flex justify-content-md-start justify-content-center flex-row-reverse pr-1 ">
-									
-										<img className="RTimg" src="./web/homepage/tutor_photo/victor.JPG" alt="" />
-										<div className="RTinfo">
-											<div className="w-100"><div className="triangle"></div><div className="title"><span>701B</span> / Victor</div></div>
-											<div className="info">PEK Basketball Competition, PEK Hall Coordinator</div>
-										</div>
-									</div>
-									<div className="col-md-6 col-12 p-0 m-0 my-auto d-flex justify-content-md-start justify-content-center pr-1 goDown">
-									
-										<img className="RTimg" src="./web/homepage/tutor_photo/Chriss.JPEG" alt="" />
-										<div className="RTinfo RTinfoR">
-											<div className="w-100 ">
-											
-											<div className="title titleR">Chriss \ <span>901A</span></div><div className="triangle triangleR"></div></div>
-											<div className="info infoR">PEK Tennis Competition, Secretary</div>
-										</div>
-									</div>
-									</div>
-									<div className="row" id="RTimg">
-									<div className="col-md-6 col-12 p-0 m-0 my-auto d-flex justify-content-md-start justify-content-center flex-row-reverse pr-1 ">
-									
-										<img className="RTimg" src="./web/homepage/tutor_photo/CK.JPG" alt="" />
-										<div className="RTinfo">
-											<div className="w-100"><div className="triangle"></div><div className="title"><span>1001B</span> / CK</div></div>
-											<div className="info">PEK Table Tennis Competition, Senior RT, Finance</div>
-										</div>
-									</div>
-									<div className="col-md-6 col-12 p-0 m-0 my-auto d-flex justify-content-md-start justify-content-center pr-1 goDown">
-									
-										<img className="RTimg" src="./web/homepage/tutor_photo/Suzy.JPG" alt="" />
-										<div className="RTinfo RTinfoR">
-											<div className="w-100 ">
-											
-											<div className="title titleR">Suzy \ <span>1101A</span></div><div className="triangle triangleR"></div></div>
-											<div className="info infoR">PEK Aquatic Meet, Hall Facilities, Hall Publicity</div>
-										</div>
-									</div>
+                                    <div className="col-md-6 col-12 p-0 m-0 my-auto d-flex justify-content-md-start justify-content-center flex-row-reverse pr-1 ">
+
+                                        <img className="RTimg" src="./web/homepage/tutor_photo/Eugene.JPG" alt="" />
+                                        <div className="RTinfo">
+                                            <div className="w-100"><div className="triangle"></div><div className="title"><span>101B</span> / Eugene</div></div>
+                                            <div className="info">Hall Book, PEK Soccer Competition, SR Mid-Autumn</div>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6 col-12 p-0 m-0 my-auto d-flex justify-content-md-start justify-content-center pr-1 goDown">
+
+                                        <img className="RTimg" src="./web/homepage/tutor_photo/Alex.JPG" alt="" />
+                                        <div className="RTinfo RTinfoR">
+                                            <div className="w-100 ">
+
+                                                <div className="title titleR">Alex \ <span>301A</span></div><div className="triangle triangleR"></div></div>
+                                            <div className="info infoR">Returning Scheme, Hall Scholarship, PEK Singing, Floor Decoration</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="row" id="RTimg">
+                                    <div className="col-md-6 col-12 p-0 m-0 my-auto d-flex justify-content-md-start justify-content-center flex-row-reverse pr-1 ">
+
+                                        <img className="RTimg" src="./web/homepage/tutor_photo/Naeem.jpg" alt="" />
+                                        <div className="RTinfo">
+                                            <div className="w-100"><div className="triangle"></div><div className="title"><span>401B</span> / Naeem</div></div>
+                                            <div className="info">Hall Discipline, PEK Photography, Integration Trip</div>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6 col-12 p-0 m-0 my-auto d-flex justify-content-md-start justify-content-center pr-1 goDown">
+
+                                        <img className="RTimg" src="./web/homepage/tutor_photo/Andrew.JPG" alt="" />
+                                        <div className="RTinfo RTinfoR">
+                                            <div className="w-100 ">
+
+                                                <div className="title titleR">Andrew \ <span>501A</span></div><div className="triangle triangleR"></div></div>
+                                            <div className="info infoR">RT(Advocacy), PEK Tug-of-War, RA Liaison, Q&A Competition</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="row" id="RTimg">
+                                    <div className="col-md-6 col-12 p-0 m-0 my-auto d-flex justify-content-md-start justify-content-center flex-row-reverse pr-1 ">
+
+                                        <img className="RTimg" src="./web/homepage/tutor_photo/victor.JPG" alt="" />
+                                        <div className="RTinfo">
+                                            <div className="w-100"><div className="triangle"></div><div className="title"><span>701B</span> / Victor</div></div>
+                                            <div className="info">PEK Basketball Competition, PEK Hall Coordinator, Food Journey</div>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6 col-12 p-0 m-0 my-auto d-flex justify-content-md-start justify-content-center pr-1 goDown">
+
+                                        <img className="RTimg" src="./web/homepage/tutor_photo/Chriss.JPG" alt="" />
+                                        <div className="RTinfo RTinfoR">
+                                            <div className="w-100 ">
+
+                                                <div className="title titleR">Chriss \ <span>901A</span></div><div className="triangle triangleR"></div></div>
+                                            <div className="info infoR">PEK Athletic Relay, Secretary, SR Spring Festival</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="row" id="RTimg">
+                                    <div className="col-md-6 col-12 p-0 m-0 my-auto d-flex justify-content-md-start justify-content-center flex-row-reverse pr-1 ">
+
+                                        <img className="RTimg" src="./web/homepage/tutor_photo/CK.JPG" alt="" />
+                                        <div className="RTinfo">
+                                            <div className="w-100"><div className="triangle"></div><div className="title"><span>1001B</span> / CK</div></div>
+                                            <div className="info">PEK Aquatic Relay, Senior RT, Finance, Academians' Talk, SRO Liaison</div>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6 col-12 p-0 m-0 my-auto d-flex justify-content-md-start justify-content-center pr-1 goDown">
+
+                                        <img className="RTimg" src="./web/homepage/tutor_photo/SzeSze.JPG" alt="" />
+                                        <div className="RTinfo RTinfoR">
+                                            <div className="w-100 ">
+
+                                                <div className="title titleR">SzeSze \ <span>1101A</span></div><div className="triangle triangleR"></div></div>
+                                            <div className="info infoR">PEK Table-Tennis, Hall Facilities, Cooking Competition</div>
+                                        </div>
+                                    </div>
 									</div>
 							</div>
 							<div></div>
