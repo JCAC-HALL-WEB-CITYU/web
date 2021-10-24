357bf2dd409361a85f6da6ba74dbda28f1f96188
JohnXW <wongkahingerv@gmail.com>
2021-10-24 17:22:50 +0800

    mt update

---
+++ src/components/hallMan/hallMan copy.jsx
@@ -0,0 +1,196 @@
+import React, { Component } from "react";
+import "./hallMan.scss";
+
+class HallMan extends React.Component {
+  render() {
+    return (
+      <div className="whiteBG2 box HallMan">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4 col-12 ">
+              <h2 className="col-12">HALL MASTER</h2>
+              <img className="" src="./web/homepage/tutor_photo/Ben.JPG" />
+              <h3 className="col-12">Prof. Ben LEUNG</h3>
+              <div className="parentP">
+                <p>
+                  Welcome to our Jockey Club Academy Hall (JCAC) big family of
+                  Academians! Here you will meet our fellow residents with
+                  diverse background from different disciplines and countries.
+                  This is a good place for you to enlarge your social circle and
+                  make more friends. Enjoy your life here!
+                </p>
+              </div>
+            </div>
+
+            <div className="col-lg-8 col-12">
+              <h2 className="col-12">RESIDENCE TUTORS</h2>
+              <div className="col-12 container">
+                <div className="row" id="RTimg">
+                  <div className="col-md-6 col-12 p-0 m-0 my-auto d-flex justify-content-md-start justify-content-center flex-row-reverse pr-1 ">
+                    <img
+                      className="RTimg"
+                      src="./web/homepage/tutor_photo/Eugene.jpg"
+                      alt=""
+                    />
+                    <div className="RTinfo">
+                      <div className="w-100">
+                        <div className="triangle"></div>
+                        <div className="title">
+                          <span>101B</span> / Eugene
+                        </div>
+                      </div>
+                      <div className="info">
+                        <b>1/F</b> | Hall Book, PEK Soccer Competition, Cooking
+                        Competition, MAC Night
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-md-6 col-12 p-0 m-0 my-auto d-flex justify-content-md-start justify-content-center pr-1 goDown">
+                    <img
+                      className="RTimg"
+                      src="./web/homepage/tutor_photo/Sherry.jpg"
+                      alt=""
+                    />
+                    <div className="RTinfo RTinfoR">
+                      <div className="w-100 ">
+                        <div className="title titleR">
+                          Sherry \ <span>301A</span>
+                        </div>
+                        <div className="triangle triangleR"></div>
+                      </div>
+                      <div className="info infoR">
+                        <b>2,3/F</b> | Advocacy, Liason, PEK Athletic Relay,
+                        Welcoming Reception
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="row" id="RTimg">
+                  <div className="col-md-6 col-12 p-0 m-0 my-auto d-flex justify-content-md-start justify-content-center flex-row-reverse pr-1 ">
+                    <img
+                      className="RTimg"
+                      src="./web/homepage/tutor_photo/Naeem1.jpg"
+                      alt=""
+                    />
+                    <div className="RTinfo">
+                      <div className="w-100">
+                        <div className="triangle"></div>
+                        <div className="title">
+                          <span>401B</span> / Naeem
+                        </div>
+                      </div>
+                      <div className="info">
+                        <b>4/F</b> | Senior RT, Hall Discipline, PEK
+                        Photography, Integration Trip
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-md-6 col-12 p-0 m-0 my-auto d-flex justify-content-md-start justify-content-center pr-1 goDown">
+                    <img
+                      className="RTimg"
+                      src="./web/homepage/tutor_photo/Amir.jpg"
+                      alt=""
+                    />
+                    <div className="RTinfo RTinfoR">
+                      <div className="w-100 ">
+                        <div className="title titleR">
+                          Amir \ <span>501A</span>
+                        </div>
+                        <div className="triangle triangleR"></div>
+                      </div>
+                      <div className="info infoR">
+                        <b>5/F</b> | Finance, PEK Tug-of-War, Mid-autumn
+                        Festival, Q&A Competition
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="row" id="RTimg">
+                  <div className="col-md-6 col-12 p-0 m-0 my-auto d-flex justify-content-md-start justify-content-center flex-row-reverse pr-1 ">
+                    <img
+                      className="RTimg"
+                      src="./web/homepage/tutor_photo/victor.JPG"
+                      alt=""
+                    />
+                    <div className="RTinfo">
+                      <div className="w-100">
+                        <div className="triangle"></div>
+                        <div className="title">
+                          <span>701B</span> / Victor
+                        </div>
+                      </div>
+                      <div className="info">
+                        <b>6,7/F</b> | PEK Hall Coordinator, PEK Basketball
+                        Competition, Welcoming Reception, SRO Liason
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-md-6 col-12 p-0 m-0 my-auto d-flex justify-content-md-start justify-content-center pr-1 goDown">
+                    <img
+                      className="RTimg"
+                      src="./web/homepage/tutor_photo/Celine.jpg"
+                      alt=""
+                    />
+                    <div className="RTinfo RTinfoR">
+                      <div className="w-100 ">
+                        <div className="title titleR">
+                          Celine \ <span>901A</span>
+                        </div>
+                        <div className="triangle triangleR"></div>
+                      </div>
+                      <div className="info infoR">
+                        <b>8,9/F</b> | Secretary, PEK Sing Con, Floor Decoration
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="row" id="RTimg">
+                  <div className="col-md-6 col-12 p-0 m-0 my-auto d-flex justify-content-md-start justify-content-center flex-row-reverse pr-1 ">
+                    <img
+                      className="RTimg"
+                      src="./web/homepage/tutor_photo/Alex.jpg"
+                      alt=""
+                    />
+                    <div className="RTinfo">
+                      <div className="w-100">
+                        <div className="triangle"></div>
+                        <div className="title">
+                          <span>1001B</span> / Alex
+                        </div>
+                      </div>
+                      <div className="info">
+                        <b>10/F</b> | Returning, Scholarship, PEK Table tennis,
+                        Academians' Talk, MAC Night
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-md-6 col-12 p-0 m-0 my-auto d-flex justify-content-md-start justify-content-center pr-1 goDown">
+                    <img
+                      className="RTimg"
+                      src="./web/homepage/tutor_photo/Sophie.jpg"
+                      alt=""
+                    />
+                    <div className="RTinfo RTinfoR">
+                      <div className="w-100 ">
+                        <div className="title titleR">
+                          Sophie \ <span>1101A</span>
+                        </div>
+                        <div className="triangle triangleR"></div>
+                      </div>
+                      <div className="info infoR">
+                        <b>11/F</b> | Facilities, Spring Festival, PEK Rowing,
+                        Food Journey, Q&A Competition
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default HallMan;
--- src/components/hallMan/hallMan.jsx
+++ src/components/hallMan/hallMan.jsx
@@ -1,117 +1,169 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
 import "./hallMan.scss";
 
-class HallMan extends React.Component
-{
-	render()
-	{
-		return (
+const HallMan = () => {
+	const peopleInfo = [
+		{
+			name:"Chester",
+			room: "101B",
+			floor: "1,2/F",
+			duties: "PEK - Soccer, Hall book,  Cooking Competition, MAC Night (Sem A)",
+			photo: "Chester.jpg",
+			position: "left"
+		},
+		{
+			name:"Beryl",
+			room: "301A",
+			floor: "3/F",
+			duties:	"PEK - Table tennis, Academians' Talk, MAC Night (Sem B), Farewell Party (Sem B)",
+			photo: "Yumeng.JPG",
+			position: "right"
+		},
+		{
+			name:"Kenny",
+			room: "401B",
+			floor: "4/F",
+			duties: "PEK -  Basketball, Common Room Cleaniness Competition (Sem A)",
+			photo: "Kenny.jpg",
+			position: "left"
+		},
+		{
+			name:"Amir",
+			room: "501A",
+			floor: "5/F",
+			duties: "PEK- Crate climbing, Welcoming Reception (Sem B), Q&A Competition (competition day)",
+			photo: "Amir.jpg",
+			position: "right"
+		},
+		{
+			name:"Carlos",
+			room: "701B",
+			floor: "6,7/F",
+			duties: "PEK - Photography, Integration Trip, Common Room Cleaniness Competition (Sem B)",
+			photo: "Carlos.jpg",
+			position: "left"
+		},
+		{
+			name:"Celine",
+			room: "901A",
+			floor: "8,9/F",
+			duties: "Hall Secretary, PEK - Sing Con, Door Decoration",
+			photo: "Celine.jpg",
+			position: "right"
+		},
+		{
+			name:"Jasmine",
+			room: "1001B",
+			floor: "10/F",
+			duties: "PEK - Athletic Meet, Farewell Party (Sem A),  Q&A Competition (preparation), RA Liason",
+			photo: "Jasmine.jpg",
+			position: "left"
+		},
+		{
+			name:"Aruna",
+			room: "1101A",
+			floor: "11/F",
+			duties: "PEK - badminton, Food Journey, 1-min Video Competition (Sem A)",
+			photo: "Aruna.jpg",
+			position: "right"
+		},
+	];
 
-			<div className="whiteBG2 box HallMan">
-
-
-				<div className="container">
-					<div className="row">
-						<div className="col-lg-4 col-12 ">
-							<h2 className="col-12">HALL MASTER</h2>
-							<img className="" src="./web/homepage/tutor_photo/Ben.JPG" />
-							<h3 className="col-12">Prof. Ben LEUNG</h3>
-							<div className="parentP">
-								<p>
-									Welcome to our Jockey Club Academy Hall (JCAC) big family of Academians! Here you will meet our fellow residents with diverse background from different disciplines and countries. This is a good place for you to enlarge your social circle and make more friends. Enjoy your life here!
-												</p></div>
+	const peopleComp = (props) =>{
+		const peopleCompLeft = ({ name, room, floor, duties, photo }) => {
+			return (
+				<div className="col-md-6 col-12 p-0 m-0 my-auto d-flex justify-content-md-start justify-content-center pr-1 flex-row-reverse">
+					<img
+						className="RTimg"
+						src={`./web/homepage/tutor_photo/${photo}`}
+						alt={photo}
+					/>
+					<div className="RTinfo">
+						<div className="w-100">
+						<div className="triangle"></div>
+						<div className="title">
+							<span>{floor}</span> / {name}
 						</div>
-
-						<div className="col-lg-8 col-12">
-							<h2 className="col-12">RESIDENCE TUTORS</h2>
-							<div className="col-12 container">
-								<div className="row" id="RTimg">
-									<div className="col-md-6 col-12 p-0 m-0 my-auto d-flex justify-content-md-start justify-content-center flex-row-reverse pr-1 ">
-									
-										<img className="RTimg" src="./web/homepage/tutor_photo/Eugene.jpg" alt="" />
-										<div className="RTinfo">
-											<div className="w-100"><div className="triangle"></div><div className="title"><span>101B</span> / Eugene</div></div>
-											<div className="info"><b>1/F</b> | Hall Book, PEK Soccer Competition, Cooking Competition, MAC Night</div>
-										</div>
-									</div>
-									<div className="col-md-6 col-12 p-0 m-0 my-auto d-flex justify-content-md-start justify-content-center pr-1 goDown">
-									
-										<img className="RTimg" src="./web/homepage/tutor_photo/Sherry.jpg" alt="" />
-										<div className="RTinfo RTinfoR">
-											<div className="w-100 ">
-											
-											<div className="title titleR">Sherry \ <span>301A</span></div><div className="triangle triangleR"></div></div>
-											<div className="info infoR"><b>2,3/F</b> | Advocacy, Liason, PEK Athletic Relay, Welcoming Reception</div>
-										</div>
-									</div>
-									</div>
-									<div className="row" id="RTimg">
-									<div className="col-md-6 col-12 p-0 m-0 my-auto d-flex justify-content-md-start justify-content-center flex-row-reverse pr-1 ">
-									
-										<img className="RTimg" src="./web/homepage/tutor_photo/Naeem1.jpg" alt="" />
-										<div className="RTinfo">
-											<div className="w-100"><div className="triangle"></div><div className="title"><span>401B</span> / Naeem</div></div>
-											<div className="info"><b>4/F</b> | Senior RT, Hall Discipline, PEK Photography, Integration Trip</div>
-										</div>
-									</div>
-									<div className="col-md-6 col-12 p-0 m-0 my-auto d-flex justify-content-md-start justify-content-center pr-1 goDown">
-									
-										<img className="RTimg" src="./web/homepage/tutor_photo/Amir.jpg" alt="" />
-										<div className="RTinfo RTinfoR">
-											<div className="w-100 ">
-											
-											<div className="title titleR">Amir \ <span>501A</span></div><div className="triangle triangleR"></div></div>
-											<div className="info infoR"><b>5/F</b> | Finance, PEK  Tug-of-War, Mid-autumn Festival, Q&A Competition</div>
-										</div>
-									</div>
-									</div>
-									<div className="row" id="RTimg">
-									<div className="col-md-6 col-12 p-0 m-0 my-auto d-flex justify-content-md-start justify-content-center flex-row-reverse pr-1 ">
-									
-										<img className="RTimg" src="./web/homepage/tutor_photo/victor.JPG" alt="" />
-										<div className="RTinfo">
-											<div className="w-100"><div className="triangle"></div><div className="title"><span>701B</span> / Victor</div></div>
-											<div className="info"><b>6,7/F</b> | PEK Hall Coordinator, PEK Basketball Competition, Welcoming Reception, SRO Liason</div>
-										</div>
-									</div>
-									<div className="col-md-6 col-12 p-0 m-0 my-auto d-flex justify-content-md-start justify-content-center pr-1 goDown">
-									
-										<img className="RTimg" src="./web/homepage/tutor_photo/Celine.jpg" alt="" />
-										<div className="RTinfo RTinfoR">
-											<div className="w-100 ">
-											
-											<div className="title titleR">Celine \ <span>901A</span></div><div className="triangle triangleR"></div></div>
-											<div className="info infoR"><b>8,9/F</b> | Secretary, PEK Sing Con, Floor Decoration</div>
-										</div>
-									</div>
-									</div>
-									<div className="row" id="RTimg">
-									<div className="col-md-6 col-12 p-0 m-0 my-auto d-flex justify-content-md-start justify-content-center flex-row-reverse pr-1 ">
-									
-										<img className="RTimg" src="./web/homepage/tutor_photo/Alex.jpg" alt="" />
-										<div className="RTinfo">
-											<div className="w-100"><div className="triangle"></div><div className="title"><span>1001B</span> / Alex</div></div>
-											<div className="info"><b>10/F</b> | Returning, Scholarship, PEK Table tennis, Academians' Talk, MAC Night</div>
-										</div>
-									</div>
-									<div className="col-md-6 col-12 p-0 m-0 my-auto d-flex justify-content-md-start justify-content-center pr-1 goDown">
-									
-										<img className="RTimg" src="./web/homepage/tutor_photo/Sophie.jpg" alt="" />
-										<div className="RTinfo RTinfoR">
-											<div className="w-100 ">
-											
-											<div className="title titleR">Sophie \ <span>1101A</span></div><div className="triangle triangleR"></div></div>
-											<div className="info infoR"><b>11/F</b> | Facilities, Spring Festival, PEK Rowing, Food Journey, Q&A Competition</div>
-										</div>
-									</div>
-									</div>
-							</div>
-							<div></div>
+						</div>
+						<div className="info">
+						<b>{room}</b> | {duties}
 						</div>
 					</div>
 				</div>
+			)
+		};
+		const peopleCompRight = ({ name, room, floor, duties, photo }) => {
+			return (
+				<div className="col-md-6 col-12 p-0 m-0 my-auto d-flex justify-content-md-start justify-content-center pr-1 goDown">
+					<img
+						className="RTimg"
+						src={`./web/homepage/tutor_photo/${photo}`}
+						alt={photo}
+					/>
+					<div className="RTinfo RTinfoR">
+						<div className="w-100 ">
+						<div className="title titleR">
+							{name} \ <span>{floor}</span>
+						</div>
+						<div className="triangle triangleR"></div>
+						</div>
+						<div className="info infoR">
+							<b>{room}</b> | {duties}
+						</div>
+					</div>
+				</div>
+			)
+		};
+		if (props.position === "left") {
+			return peopleCompLeft(props)
+		} else {
+			return peopleCompRight(props)
+		}
+	};
+	return (
+		<div className="whiteBG2 box HallMan">
+			<div className="container">
+				<div className="row">
+				<div className="col-lg-4 col-12 ">
+					<h2 className="col-12">HALL MASTER</h2>
+					<img className="" src="./web/homepage/tutor_photo/Ben.JPG" />
+					<h3 className="col-12">Prof. Ben LEUNG</h3>
+					<div className="parentP">
+					<p>
+						Welcome to our Jockey Club Academy Hall (JCAC) big family of
+						Academians! Here you will meet our fellow residents with
+						diverse background from different disciplines and countries.
+						This is a good place for you to enlarge your social circle and
+						make more friends. Enjoy your life here!
+					</p>
+					</div>
+				</div>
+	
+				<div className="col-lg-8 col-12">
+					<h2 className="col-12">RESIDENCE TUTORS</h2>
+					<div className="col-12 container">
+					<div className="row" id="RTimg">
+						{peopleComp(peopleInfo[0])}
+						{peopleComp(peopleInfo[1])}
+					</div>
+					<div className="row" id="RTimg">
+						{peopleComp(peopleInfo[2])}
+						{peopleComp(peopleInfo[3])}
+					</div>
+					<div className="row" id="RTimg">
+						{peopleComp(peopleInfo[4])}
+						{peopleComp(peopleInfo[5])}
+					</div>
+					<div className="row" id="RTimg">
+						{peopleComp(peopleInfo[6])}
+						{peopleComp(peopleInfo[7])}
+					</div>
+					</div>
+					<div></div>
+				</div>
+				</div>
 			</div>
-		);
-	}
+		</div>
+	)
 }
 export default HallMan;
